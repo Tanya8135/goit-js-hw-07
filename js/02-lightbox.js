12ef3ff36cd78185f3c4c4c2eeaedeac970b94b2
tanya8135 <108739694+Tanya8135@users.noreply.github.com>
2023-03-31 21:51:26 +0300

    done hw-2-2
    add simpleLightBox (library)

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,27 +1,27 @@
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
-
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 const galleryList = document.querySelector('.gallery'); /* клас галереї */
 
-// new SimpleLightbox('.some-element a', { captionsData });
-const lightbox = new SimpleLightbox('.gallery__link', {
-  captions: true,
-  captionsData: 'title', /* от куда брать название */
-  captionDelay: 250, /* задержка */
-  captionPosition: 'bottom', /* размещение внизу */
-}); /* работа с библиотекой */
 
-const galleryMarkup = galleryItems.map(item => /* створення масиву, додавання розмітки, перетворення в строку */
-  `<li class="gallery__item">
-<a class="gallery__link" href="large-image.jpg">
-   <img class="gallery__image" src="small-image.jpg" alt="Image description" />
-</a>
-</li>`).join('');
+const galleryMarkup = galleryItems.map(({ original, preview, description }) => { /* створення масиву, додавання розмітки, перетворення в строку */
+  return `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" title="${description}" />
+      </a>
+    </li>`;
+  }).join('');
 
 galleryList.insertAdjacentHTML('beforeend', galleryMarkup); /* добавляем список <li> в <ul> */
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: 'title', /* от куда брать название */
+    captionDelay: 250, /* задержка */
+    captionsPosiyion: 'bottom', /* размещение внизу */
+}); /* работа с опцией из библиотеки */
+
 
 document.addEventListener('keydown', function (event) { /* закрытие "модалки" с Escape */
   if (event.code === 'Escape') { /* (code) - повертає фізичну клавішу */
